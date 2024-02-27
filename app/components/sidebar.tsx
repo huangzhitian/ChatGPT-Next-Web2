cfd7f28583288de9e51d2416a06a8d1ff1ab406a
Feng <129321746+huangzhitian@users.noreply.github.com>
2024-02-27 16:50:33 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,9 +4,9 @@ import styles from "./home.module.scss";
 
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/github.svg";
-import ChatGptIcon from "../icons/chatgpt.svg";
-import AddIcon from "../icons/add.svg";
+import GithubIcon from "../icons/qq.svg"; 
+import ChatGptIcon from "../icons/chatgpt.svg"; 
+import AddIcon from "../icons/add.svg"; 
 import CloseIcon from "../icons/close.svg";
 import DeleteIcon from "../icons/delete.svg";
 import MaskIcon from "../icons/mask.svg";
